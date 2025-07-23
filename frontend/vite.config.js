9d2dc5672a2cdbdda19ddadcf9442fbeeec34fe8
JulianBati92 <113951097+JulianBati92@users.noreply.github.com>
2025-07-23 02:04:23 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000'
-    }
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 });
